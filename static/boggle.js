67be22fe3ec7851beb2f511fe9c21d56ed58b9b1
Winnie Chou <winnie.chou.mb@gmail.com>
2020-12-04 18:49:08 -0600

    code review comments and edits

--- static/boggle.js
+++ static/boggle.js
@@ -19,7 +19,7 @@ async function start() {
   displayBoard(board);
 
   //add event listener for submit form
-  $("button").on("click", submitWord)
+  $form.on("submit", submitWord)
 }
 
 /** Display board */
@@ -28,11 +28,11 @@ function displayBoard(board) {
   let $tbody = $("tbody");
   $tbody.empty();
   // loop over board and create the DOM tr/td structure
-  for(let row of board){
+  for (let row of board) {
     let newRow = $('<tr>')
-    for(let ltr of row){
-      let new_ltr = $('<td>').text(ltr);
-      newRow.append(new_ltr);
+    for (let ltr of row) {
+      let newLtr = $('<td>').text(ltr);
+      newRow.append(newLtr);
     }
     $tbody.append(newRow);
   }
@@ -42,25 +42,28 @@ function displayBoard(board) {
  *  Returns: 
  */
 
-async function submitWord(evt){
+async function submitWord(evt) {
   evt.preventDefault();
   let word = $wordInput.val().toUpperCase();
-  let response = await axios.post("/api/score-word", {gameId, word});
+  let response = await axios.post("/api/score-word", { gameId, word });
   // console.log(response.data);
-  playUpdate(response.data["result"], word);
+  playUpdate(response.data.result, word);
 }
 
 /** Checks if word submitted is a legal play and update DOM with play */
 
-function playUpdate(result, word){
+function playUpdate(result, word) {
   // console.log(word);
   // console.log(result);
-  if(result === "ok"){
+  if (result === "ok") {
+    $message.empty();
     $playedWords.append($(`<li>${word}</li>`));
   }
-  else{
-    result = result.split('-').join(' ');
-    $message.text(`${word} is ${result}`);
+  else if (result === "not-on-board") {
+    $message.text(`${word} is not on board`);
+  }
+  else if (result === "not-word") {
+    $message.text(`${word} is not a word`);
   }
 }
 
